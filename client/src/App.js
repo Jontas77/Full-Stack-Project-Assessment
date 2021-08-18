fab8702d98c979a57e99597b3a22f039478da7e3
Jontas77 <mrjbheugh@gmail.com>
2021-08-18 22:56:08 +0200

    Completed level 300

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+//Import components
+import Header from "./components/Header";
+import AddVideo from "./components/AddVideo";
+import AllVideos from "./components/AllVideos";
+
+const App = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/`)
+    .then(res => res.json())
+    .then(data => setData(data))
+  }, []);
+
+ 
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
+      <Header />
+      <main>
+        <AddVideo search={search} handleSearch={handleSearch} />
+        <div className="d-flex justify-content-center flex-wrap">
+          {data.filter(val => {
+            return (
+           search === "" ? val : (val.title.toLowerCase().includes(search.toLowerCase()) ? val : null) 
+            )
+          }).map((value, idx) => (
+            <AllVideos data={value} key={idx} />
+          ))}
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
+
